075ee887c91965cad0731e74e1ebd1166228d491
Mor Mordechai Ben <mor6686@gmail.com>
2022-05-20 13:29:10 +0300

    hiding address bar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,25 @@
 import RoundScreenMobile from './screens/rounde/RoundScreen.mobile';
 
 // import StopWatchBrain from './components/stopWatch/StopWatchBrain';
+
+function hideAddressBar() {
+  if (!window.location.hash) {
+    if (document.height < window.outerHeight) {
+      document.body.style.height = window.outerHeight + 50 + 'px';
+    }
+
+    setTimeout(function () {
+      window.scrollTo(0, 1);
+    }, 50);
+  }
+}
+
+window.addEventListener('load', function () {
+  if (!window.pageYOffset) {
+    hideAddressBar();
+  }
+});
+window.addEventListener('orientationchange', hideAddressBar);
 function App() {
   // const [width, height] = useWindowSize();
 
